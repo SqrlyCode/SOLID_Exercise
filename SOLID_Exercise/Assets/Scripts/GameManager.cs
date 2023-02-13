526cdc90ae806b945865dd4ed271a4c5dfea1ae5
Andreas Metz <sqrlycode@gmail.com>
2023-02-13 19:41:15 +0100

    Added GameManager to spawn Objects

---
+++ SOLID_Exercise/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _amountOfShapesToSpawn = 10;
+    [SerializeField] private Rect _spawnArea;
+    [SerializeField] private GameObject[] _shapePrefabs;
+    
+    
+    void Start()
+    {
+        for (int i = 0; i < _amountOfShapesToSpawn; i++)
+            SpawnShape();
+    }
+
+    private void SpawnShape()
+    {
+        Vector2 rndPos = new Vector2(Random.Range(-_spawnArea.width/2, _spawnArea.width/2), Random.Range(-_spawnArea.height/2, _spawnArea.height/2));
+        
+        int rndShapeIndex = Random.Range(0, _shapePrefabs.Length);
+        Instantiate(_shapePrefabs[rndShapeIndex], rndPos, Quaternion.identity);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0,1,0,0.25f);
+        Gizmos.DrawCube(_spawnArea.position, new Vector3(_spawnArea.width, _spawnArea.height, 0.1f));
+    }
+}
