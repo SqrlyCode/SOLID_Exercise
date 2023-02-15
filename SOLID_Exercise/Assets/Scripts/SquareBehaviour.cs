6b3c55e184d661c87db46aaf13b0791d91d7b1ef
Andreas Metz <sqrlycode@gmail.com>
2023-02-15 16:53:30 +0100

    Added Square Behaviour and Projectiles

--- SOLID_Exercise/Assets/Scripts/SquareBehaviour.cs
+++ SOLID_Exercise/Assets/Scripts/SquareBehaviour.cs
@@ -1,12 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SquareBehaviour : MonoBehaviour, IShapeBehaviour
 {
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private float _projectileRespawnCooldown = 1;
+
+    private float[] _projectileRespawnTimer;
+    private List<SquareProjectile> _projectiles;//0:top, 1:right, 2:bot, 3:left 
+    
     public Vector2 _Position => transform.position;
+
+    private void Awake()
+    {
+        _projectileRespawnTimer = new float[4];
+        _projectiles = new List<SquareProjectile> {null,null,null,null};
+        _projectiles.Capacity = 4;
+        for (int i = 0; i < 4; i++)
+        {
+            SpawnProjectile(i);
+        }
+
+    }
+
+    private void Update()
+    {
+        UpdateProjectilePositions();
+        
+        //Respawn Projectiles
+        for (int i = 0; i < _projectiles.Count; i++)
+        {
+            if (_projectiles[i] == null)
+            {
+                _projectileRespawnTimer[i] += Time.deltaTime;
+                if (_projectileRespawnTimer[i] >= _projectileRespawnCooldown)
+                {
+                    SpawnProjectile(i);
+                    _projectileRespawnTimer[i] = 0;
+                }
+            }
+        }
+    }
+
     public void ShootAtPosition(Vector2 position)
     {
-        throw new System.NotImplementedException();
+        foreach (var projectile in _projectiles)
+        {
+            projectile?.SetDestination(position);
+        }
+    }
+
+    private void SpawnProjectile(int index)
+    {
+        _projectiles[index] = Instantiate(_projectilePrefab, transform.position, Quaternion.identity).GetComponent<SquareProjectile>();
+        _projectiles[index].Init(this);
+        _projectiles[index].destroyed += SquareProjectile_Destroyed;
+    }
+    
+    private void UpdateProjectilePositions()
+    {
+        _projectiles[0]?.UpdateOriginPosition(transform.position + Vector3.up);
+        _projectiles[1]?.UpdateOriginPosition(transform.position + Vector3.right);
+        _projectiles[2]?.UpdateOriginPosition(transform.position + Vector3.down);
+        _projectiles[3]?.UpdateOriginPosition(transform.position + Vector3.left);
+    }
+    
+    private void SquareProjectile_Destroyed(SquareProjectile projectile)
+    {
+        int index = _projectiles.IndexOf(projectile);
+        _projectiles[index] = null;
     }
 }
---
+++ SOLID_Exercise/Assets/Scripts/SquareProjectile.cs
@@ -0,0 +1,123 @@
+using System;
+using DG.Tweening;
+using UnityEditor;
+using UnityEngine;
+
+public class SquareProjectile : MonoBehaviour, IProjectile
+{
+    public SquareProjectileState _State { get; private set; }
+    public int _Damage => _damage;
+    public IShapeBehaviour _Creator { get; private set; }
+    public event Action<SquareProjectile> destroyed;
+        
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private float _moveSpeed = 8;
+    [SerializeField] private float _rotationSpeed = 180;
+    
+    
+    private Vector2 _destination;
+    private Vector2 _originPosition;
+    
+    public enum SquareProjectileState
+    {
+        Idle,
+        FlyingTowardsDestination,
+        Returning
+    }
+
+
+    public void Init(IShapeBehaviour creator)
+    {
+        _State = SquareProjectileState.Idle;
+        _Creator = creator;
+        Vector3 initScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+        transform.DOScale(initScale, 0.2f);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, -_rotationSpeed * Time.deltaTime);
+        
+        switch (_State)
+        {
+            case SquareProjectileState.Idle:
+                UpdateIdle();
+                break;
+            case SquareProjectileState.FlyingTowardsDestination:
+                UpdateFlyingTowardsDestination();
+                break;
+            case SquareProjectileState.Returning:
+                UpdateReturning();
+                break;
+        }
+    }
+
+    private void UpdateIdle()
+    {
+        transform.position = _originPosition;
+    }
+
+    private void UpdateFlyingTowardsDestination()
+    {
+    }
+
+    private void UpdateReturning()
+    {
+        Vector2 dir = (_originPosition - (Vector2)transform.position).normalized;
+        // transform.position = (Vector2)transform.position + dir * (Time.deltaTime * _moveSpeed);
+        float t = _moveSpeed / Vector2.Distance(transform.position, _originPosition);//For constant movespeed
+        transform.position = Vector3.Lerp(transform.position, _originPosition, 0.1f + Time.deltaTime * t);
+        if (Vector2.Distance(transform.position, _originPosition) < 0.1f)
+        {
+            _State = SquareProjectileState.Idle;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        ShapeMotor shapeMotor = col.GetComponent<ShapeMotor>();
+        IShapeBehaviour shapeBehaviour = col.GetComponent<IShapeBehaviour>();
+        IProjectile projectile = col.GetComponent<IProjectile>();
+
+        if (projectile!= null && projectile.GetType() != this.GetType())
+        {
+            Die();
+        }
+        //If ShapeMotor is on Object and Shapebehavior is something else than the Type that fired. i.e. triangles can not hit other triangles
+        else if (shapeBehaviour != null && shapeBehaviour.GetType() !=  _Creator.GetType())
+        {
+            shapeMotor._Health -= _Damage;
+            Die();
+        }
+        else if (col.CompareTag("Wall"))
+            Die();
+    }
+
+    public void SetDestination(Vector3 destination)
+    {
+        if (_State == SquareProjectileState.Idle)
+        {
+            _destination = destination;
+            _State = SquareProjectileState.FlyingTowardsDestination;
+            
+            float moveDuration = Vector2.Distance(transform.position, _destination) / _moveSpeed;
+            transform.DOMove(_destination, moveDuration)
+                // .SetSpeedBased(true)
+                .SetEase(Ease.OutSine)
+                .OnComplete(()=>_State = SquareProjectileState.Returning);
+        }
+    }
+
+    public void UpdateOriginPosition(Vector2 originPos)
+    {
+        _originPosition = originPos;
+    }
+
+    private void Die()
+    {
+        DOTween.Kill(transform);
+        Destroy(gameObject);
+        destroyed?.Invoke(this);
+    }
+}
